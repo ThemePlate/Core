a9aa30685df77bcebbd4ab2eb8eb55f9375f860e
kermage <genealyson.torcende@gmail.com>
2022-06-05 22:48:59 +0800

    use Tester and add tests for the helpers

---
+++ tests/Helper/FormTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @package ThemePlate
+ */
+
+namespace Tests\Helper;
+
+use PHPUnit\Framework\TestCase;
+use ThemePlate\Core\Field;
+use ThemePlate\Core\Helper\Form;
+
+class FormTest extends TestCase {
+	public function test_make_field(): void {
+		$namespace = 'ThemePlate\Core\Field';
+
+		foreach ( glob( dirname( __FILE__, 3 ) . '/src/Field/*.php', GLOB_MARK ) as $file ) {
+			$base  = basename( $file, '.php' );
+			$type  = strtolower( $base );
+			$field = Form::make_field( 'test', compact( 'type' ) );
+
+			$this->assertInstanceOf( Field::class, $field );
+			$this->assertSame( $namespace . '\\' . $base, get_class( $field ) );
+		}
+	}
+}
