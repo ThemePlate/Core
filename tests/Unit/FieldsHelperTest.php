1103ed8d9ed25aa1dad927e4863160d42f3bfe69
kermage <genealyson.torcende@gmail.com>
2022-06-21 23:21:43 +0800

    add test for getting defaults

---
+++ tests/Unit/FieldsHelperTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * @package ThemePlate
+ */
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use ThemePlate\Core\Helper\FieldsHelper;
+use ThemePlate\Core\Helper\FormHelper;
+
+class FieldsHelperTest extends TestCase {
+	public function for_getting_default(): array {
+		// phpcs:disable WordPress.Arrays.MultipleStatementAlignment.DoubleArrowNotAligned
+		return array(
+			'with a text type field' => array(
+				'text',
+				array(),
+				'',
+			),
+			'with a text have default' => array(
+				'text',
+				array( 'default' => 'test' ),
+				'test',
+			),
+			'with a group no fields' => array(
+				'group',
+				array(),
+				'',
+			),
+			'with a group has fields' => array(
+				'group',
+				array(
+					'fields' => array(
+						'test' => array( 'type' => 'text' ),
+					),
+				),
+				array( 'test' => '' ),
+			),
+			'with fields have default' => array(
+				'group',
+				array(
+					'default' => array(
+						'test' => 'this',
+					),
+					'fields' => array(
+						'test' => array(
+							'type' => 'text',
+						),
+						'another' => array(
+							'type' => 'text',
+							'default' => 'one',
+						),
+					),
+				),
+				array(
+					'test' => 'this',
+					'another' => 'one',
+				),
+			),
+		);
+		// phpcs:enable WordPress.Arrays.MultipleStatementAlignment.DoubleArrowNotAligned
+	}
+
+	/**
+	 * @dataProvider for_getting_default
+	 */
+	public function test_getting_default( string $type, array $config, $expected ): void {
+		$field = FormHelper::make_field( 'test', array_merge( $config, compact( 'type' ) ) );
+
+		$this->assertSame( $expected, FieldsHelper::get_default( $field ) );
+	}
+}
