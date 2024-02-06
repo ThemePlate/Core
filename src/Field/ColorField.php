195ca5ca8ffa725c42d402e570de8ba8df6701cc
kermage <genealyson.torcende@gmail.com>
2024-02-06 21:32:36 +0800

    repeatable color default

--- src/Field/ColorField.php
+++ src/Field/ColorField.php
@@ -15,13 +15,19 @@ class ColorField extends Field {
 
 	public function render( $value ): void {
 
+		$default = $this->get_config( 'default' );
+
+		if ( $this->get_config( 'repeatable' ) && is_array( $default ) ) {
+			$default = $default[0];
+		}
+
 		echo '<input
 				type="text"
 				name="' . esc_attr( $this->get_config( 'name' ) ) . '"
 				id="' . esc_attr( $this->get_config( 'id' ) ) . '"
 				class="themeplate-color-picker"
 				value="' . esc_attr( $value ) . '"
-				' . ( esc_attr( $this->get_config( 'default' ) ) ? ' data-default-color="' . esc_attr( $this->get_config( 'default' ) ) . '"' : '' );
+				' . ( $default ? ' data-default-color="' . esc_attr( $default ) . '"' : '' );
 		if ( ! empty( $this->get_config( 'options' ) ) ) {
 			$values = wp_json_encode( $this->get_config( 'options' ) );
 
