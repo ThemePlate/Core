50e9837d55e08e943cc106dfebd452345c3d2f08
kermage <genealyson.torcende@gmail.com>
2024-06-03 22:37:51 +0800

    callable 'options' as transformer

--- src/Field/HtmlField.php
+++ src/Field/HtmlField.php
@@ -13,7 +13,26 @@ use ThemePlate\Core\Field;
 
 class HtmlField extends Field {
 
+	protected function initialize(): void {
+
+		$options = $this->get_config( 'options' );
+
+		if ( empty( $options ) ) {
+			return;
+		}
+
+		if ( isset( $options[0] ) && is_callable( $options[0] ) ) {
+			$this->config['options'] = $options[0];
+		}
+
+	}
+
+
 	private function handle( $value ): string {
+
+		if ( is_callable( $this->get_config( 'options' ) ) ) {
+			return call_user_func( $this->get_config( 'options' ), $value );
+		}
 
 		if ( is_string( $value ) ) {
 			return $value;
