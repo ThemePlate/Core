4c72e4c91e5de4d71b4c6f764bfdcccbe9bec2d9
kermage <genealyson.torcende@gmail.com>
2024-06-03 22:02:39 +0800

    handle value to return as string

--- src/Field/HtmlField.php
+++ src/Field/HtmlField.php
@@ -13,9 +13,24 @@ use ThemePlate\Core\Field;
 
 class HtmlField extends Field {
 
+	private function handle( $value ): string {
+
+		if ( is_string( $value ) ) {
+			return $value;
+		}
+
+		if ( is_scalar( $value ) || null === $value ) {
+			return (string) $value;
+		}
+
+		return wp_json_encode( $value );
+
+	}
+
+
 	public function render( $value ): void {
 
-		echo wp_kses_post( $value );
+		echo wp_kses_post( $this->handle( $value ) );
 
 	}
 
