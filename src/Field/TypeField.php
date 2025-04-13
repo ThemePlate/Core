2fc7076e1e22ca6e0b95733452a817f22ad6e985
kermage <genealyson.torcende@gmail.com>
2025-04-13 23:48:44 +0800

    prefixed text for user and term

--- src/Field/TypeField.php
+++ src/Field/TypeField.php
@@ -12,8 +12,11 @@ namespace ThemePlate\Core\Field;
 use ThemePlate\Core\Field;
 use ThemePlate\Core\Helper\AssetsHelper;
 use ThemePlate\Core\Helper\MainHelper;
+use WP_Post;
 use WP_Query;
+use WP_Term;
 use WP_Term_Query;
+use WP_User;
 use WP_User_Query;
 
 class TypeField extends Field {
@@ -147,28 +150,16 @@ class TypeField extends Field {
 	}
 
 
-	private static int $count      = 10;
-	private static array $prefixes = array();
+	private static int $count = 10;
 
-
-	private static function get_prefix( int $id, array $options ): string {
-
-		$prefix = '';
-
-		if ( is_array( $options['post_type'] ) && 1 < count( $options['post_type'] ) ) {
-			$type = get_post_type( $id );
-
-			if ( ! array_key_exists( $type, self::$prefixes ) ) {
-				$object                  = get_post_type_object( $type );
-				self::$prefixes[ $type ] = $object->labels->singular_name;
-			}
-
-			$prefix = self::$prefixes[ $type ] . ' | ';
-		}
-
-		return $prefix;
-
-	}
+	/**
+	 * @var array<string, array<string, string>>
+	 */
+	private static array $prefixes = array(
+		'post' => array(),
+		'user' => array(),
+		'term' => array(),
+	);
 
 
 	// phpcs:disable WordPress.Security.NonceVerification
@@ -185,12 +176,12 @@ class TypeField extends Field {
 		$defaults = array(
 			'post_status'    => 'publish',
 			's'              => $_GET['search'] ?? '',
-			'fields'         => 'ids',
 			'posts_per_page' => isset( $_GET['ids__in'] ) ? -1 : self::$count,
 			'post__in'       => $_GET['ids__in'] ?? '',
 		);
+		$is_multi = is_array( $_GET['options']['post_type'] ) && 1 < count( $_GET['options']['post_type'] );
 
-		if ( is_array( $_GET['options']['post_type'] ) && 1 < count( $_GET['options']['post_type'] ) ) {
+		if ( $is_multi ) {
 			$defaults['orderby'] = array(
 				'post_type' => 'ASC',
 			);
@@ -203,15 +194,39 @@ class TypeField extends Field {
 		}
 
 		foreach ( $query->posts as $post ) {
+			/** @var WP_Post $post */
 			$return['results'][] = array(
-				'id'   => $post,
-				'text' => self::get_prefix( $post, $_GET['options'] ) . get_the_title( $post ),
+				'id'   => $post->ID,
+				'text' => self::get_post_text( $post, $is_multi ),
 			);
 		}
 
 		echo wp_json_encode( $return );
 
 		wp_die();
+
+	}
+
+
+	private static function get_post_text( WP_Post $post, bool $is_multi ): string {
+
+		if ( ! $is_multi ) {
+			return $post->post_title;
+		}
+
+		$type = $post->post_type;
+
+		if ( ! array_key_exists( $type, self::$prefixes['post'] ) ) {
+			$object = get_post_type_object( $type );
+
+			if ( null === $object ) {
+				return $post->post_title;
+			}
+
+			self::$prefixes['post'][ $type ] = $object->labels->singular_name;
+		}
+
+		return self::$prefixes['post'][ $type ] . ' | ' . $post->post_title;
 
 	}
 
@@ -228,10 +243,11 @@ class TypeField extends Field {
 		);
 		$defaults = array(
 			'search'  => isset( $_GET['search'] ) ? '*' . $_GET['search'] . '*' : '',
-			'fields'  => array( 'ID', 'display_name' ),
 			'number'  => isset( $_GET['ids__in'] ) ? -1 : self::$count,
 			'include' => $_GET['ids__in'] ?? '',
 		);
+		$roles    = $_GET['options']['role'] ?? array();
+		$is_multi = empty( $roles ) ? true : is_array( $roles ) && 1 < count( $roles );
 		$query    = new WP_User_Query( array_merge( $defaults, $_GET['options'] ?? array(), $_GET['_page'] ) );
 
 		if ( $_GET['_page']['paged'] < ceil( $query->get_total() / self::$count ) ) {
@@ -239,15 +255,44 @@ class TypeField extends Field {
 		}
 
 		foreach ( $query->get_results() as $user ) {
+			/** @var WP_User $user */
 			$return['results'][] = array(
 				'id'   => $user->ID,
-				'text' => $user->display_name,
+				'text' => self::get_user_text( $user, $roles, $is_multi ),
 			);
 		}
 
 		echo wp_json_encode( $return );
 
 		wp_die();
+
+	}
+
+
+	/**
+	 * @param string[] $roles
+	 */
+	private static function get_user_text( WP_User $user, array $roles, bool $is_multi ): string {
+
+		if ( ! $is_multi ) {
+			return $user->display_name;
+		}
+
+		if ( empty( self::$prefixes['user'] ) ) {
+			self::$prefixes['user'] = wp_roles()->get_names();
+		}
+
+		if ( array() === $roles ) {
+			$roles = $user->roles;
+		}
+
+		foreach ( $roles as $role ) {
+			if ( array_key_exists( $role, self::$prefixes['user'] ) ) {
+				return self::$prefixes['user'][ $role ] . ' | ' . $user->display_name;
+			}
+		}
+
+		return $user->display_name;
 
 	}
 
@@ -265,28 +310,53 @@ class TypeField extends Field {
 		$offset   = ( $_GET['_page']['paged'] > 0 ) ? self::$count * ( $_GET['_page']['paged'] - 1 ) : 1;
 		$defaults = array(
 			'search'  => $_GET['search'] ?? '',
-			'fields'  => 'id=>name',
 			'number'  => isset( $_GET['ids__in'] ) ? 0 : self::$count,
 			'include' => $_GET['ids__in'] ?? '',
 			'offset'  => $offset,
 		);
 		$total    = wp_count_terms( $_GET['options'] ?? array() );
+		$taxonomy = $_GET['options']['taxonomy'] ?? array();
+		$is_multi = empty( $taxonomy ) ? true : is_array( $taxonomy ) && 1 < count( $taxonomy );
 		$query    = new WP_Term_Query( array_merge( $defaults, $_GET['options'] ?? array() ) );
 
 		if ( ! is_wp_error( $total ) && $_GET['_page']['paged'] < ceil( (int) $total / self::$count ) ) {
 			$return['pagination']['more'] = true;
 		}
 
-		foreach ( (array) $query->get_terms() as $id => $name ) {
+		foreach ( (array) $query->get_terms() as $term ) {
+			/** @var WP_Term $term */
 			$return['results'][] = array(
-				'id'   => $id,
-				'text' => $name,
+				'id'   => $term->term_id,
+				'text' => self::get_term_text( $term, $is_multi ),
 			);
 		}
 
 		echo wp_json_encode( $return );
 
 		wp_die();
+
+	}
+
+
+	private static function get_term_text( WP_Term $term, bool $is_multi ): string {
+
+		if ( ! $is_multi ) {
+			return $term->name;
+		}
+
+		$tax = $term->taxonomy;
+
+		if ( ! array_key_exists( $tax, self::$prefixes['term'] ) ) {
+			$object = get_taxonomy( $tax );
+
+			if ( false === $object ) {
+				return $term->name;
+			}
+
+			self::$prefixes['term'][ $tax ] = $object->labels->singular_name;
+		}
+
+		return self::$prefixes['term'][ $tax ] . ' | ' . $term->name;
 
 	}
 
